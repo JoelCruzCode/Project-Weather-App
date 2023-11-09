2ec8669d180b62c09b353425334e0a90c159a0cd
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-11-08 19:07:17 -0800

    display weather properties to interface

--- src/modules/userInterface.js
+++ src/modules/userInterface.js
@@ -8,13 +8,23 @@ function loadUserInterface() {
   // p.textContent = "is this on?";
   // content.appendChild(p);
   let type;
-
   const weatherForm = document.querySelector(".weather-form");
   const currentRadioBtn = document.querySelector("#current-api");
   const forecastRadioBtn = document.querySelector("#forecast-api");
   const weatherFormBtn = document.querySelector(".weather-form-btn");
   const daysElements = document.querySelectorAll(".days");
   const hoursElements = document.querySelectorAll(".hours");
+  //
+
+  const condition = document.querySelector(".condition");
+  const location = document.querySelector(".location");
+  const timeDiv = document.querySelector(".time");
+  const temperature = document.querySelector(".temperature");
+  //
+  const feelsLike = document.querySelector(".feelsLike");
+  const humidity = document.querySelector(".humidity");
+  const chanceOfRain = document.querySelector(".chance-of-rain");
+  const windSpeed = document.querySelector(".wind-speed");
 
   function checkRadioButton() {
     const radioElements = [...daysElements, ...hoursElements];
@@ -27,30 +37,50 @@ function loadUserInterface() {
     }
   }
 
-  function validateInputs() {}
-
-  function submitForm() {
+  async function submitForm() {
     if (validateForm(weatherForm)) {
       const location = document.querySelector("#location").value;
       const days = document.querySelector("#days").value;
       const time = document.querySelector("#hours").value;
       console.log(location, days, time, type);
 
-      checkWeather(location, { type: type, days: days, time: time });
+      const weatherData = await checkWeather(location, {
+        type: type,
+        days: days,
+        time: time,
+      });
+
+      console.log("weatherData: ", weatherData);
+      return weatherData;
     }
   }
 
-  weatherForm.addEventListener("click", function (e) {
-    // e.preventDefault();
+  function displayWeather(data) {
+    console.log("data: ", data);
+    console.log(data.temperature);
+    condition.textContent = data.condition.text;
+    feelsLike.textContent = data.feelsLike;
+    humidity.textContent = data.humidity;
+    temperature.textContent = data.temperature;
+    windSpeed.textContent = data.wind_mph;
+    chanceOfRain.textContent = data.chanceOfRainToday;
+    location.textContent = data.localtime;
+  }
+
+  weatherForm.addEventListener("click", function () {
     checkRadioButton();
   });
 
-  weatherFormBtn.addEventListener("click", function (e) {
+  weatherFormBtn.addEventListener("click", async function (e) {
     e.preventDefault();
-    submitForm();
+    let data = await submitForm();
+    displayWeather(data);
   });
 
-  // checkWeather("Los angeles", { type: "forecast", days: 3, time: 0 });
+  weatherForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // displayWeather(weatherData);
+  });
 }
 
 export default loadUserInterface;
